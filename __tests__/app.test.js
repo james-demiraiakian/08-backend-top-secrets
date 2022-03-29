835f8ee72a234d0ba392a19fba55fbec24cf3bf9
James Demiraiakian <james.armen@gmail.com>
2022-03-29 15:03:35 -0700

    added delete test and route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -43,4 +43,43 @@ describe('08-backend-top-secret routes', () => {
 
     expect(res.body).toEqual({ message: 'Signed in successfully', user });
   });
+
+  it('returns 401 on an invalid email', async () => {
+    await request(app).post('/api/v1/users').send(mockUser);
+    const { email, password } = mockUser;
+    await signIn({ email, password });
+
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ email: 'qwerty@ewewe.com', password });
+
+    expect(res.body).toEqual({ message: 'invalid email', status: 401 });
+  });
+
+  it('returns 401 on ', async () => {
+    await request(app).post('/api/v1/users').send(mockUser);
+    const { email, password } = mockUser;
+    await signIn({ email, password });
+
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ email, password: 'wdwadasdwasdada' });
+
+    expect(res.body).toEqual({ message: 'Invalid password', status: 401 });
+  });
+
+  it('logs out via delete function', async () => {
+    await request(app).post('/api/v1/users').send(mockUser);
+    const { email, password } = mockUser;
+    const user = await signIn({ email, password });
+
+    console.log(user.body);
+
+    const res = await request(app).delete('/api/v1/users/sessions');
+
+    expect(res.body).toEqual({
+      success: true,
+      message: 'Signed out',
+    });
+  });
 });
