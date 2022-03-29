243c75cf6396de3122409c314ccce82f20ede52b
James Demiraiakian <james.armen@gmail.com>
2022-03-29 11:59:06 -0700

    wrote test for create user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,13 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const mockUser = {
+  firstName: 'User',
+  lastName: 'Name',
+  email: 'test@defense.gov',
+  password: '12345678',
+};
+
 describe('08-backend-top-secret routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -10,5 +17,19 @@ describe('08-backend-top-secret routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('creates a new user', async () => {
+    const res = await (
+      await request(app).post('/api/v1/users')
+    ).setEncoding(mockUser);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(Number),
+      firstName,
+      lastName,
+      email,
+    });
   });
 });
